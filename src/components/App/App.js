3d704c9814c5e060abba80d89208a664a3139e72
Keegan Odell <Keegan.Odell@icloud.com>
2023-05-15 07:27:48 -0500

    fixed clean and commented on app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,17 +6,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import MovieDetails from '../MovieDetails/MovieDetails.jsx';
 
 function App() {
+
+	//dispatch allows us to use it in our useEffect
 	const dispatch = useDispatch();
+
+	//this grabs movies from the store
 	const movies = useSelector((store) => store.movies);
 
+	//this populates our movies array using redux and saga
 	useEffect(() => {
 		dispatch({
 			type: 'FETCH_MOVIES',
 		});
 	}, []);
 
-	console.log(movies);
-
+	//this is what shows up on our DOM
+	//we look through our movies and create a route path for each movie.title
+	//we pass our MovieDetails component a prop of ID so we can properly populate 
+	//the item with the correct data
 	return (
 		<div className='App'>
 			<h1>The Movies Saga!</h1>
